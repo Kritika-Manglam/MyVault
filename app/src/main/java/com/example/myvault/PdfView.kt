67882d013e2f5f8409f575b72a6c00033066d5c5
KritikaManglam <22je05012iitism.ac.in>
2023-03-03 09:05:26 +0530

    Final Commit

---
+++ app/src/main/java/com/example/myvault/PdfView.kt
@@ -0,0 +1,11 @@
+package com.example.myvault
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PdfView : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pdf_view)
+    }
+}
---
+++ app/src/main/java/com/example/myvault/ThirdPage.kt
@@ -0,0 +1,11 @@
+package com.example.myvault
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ThirdPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_third_page)
+    }
+}
