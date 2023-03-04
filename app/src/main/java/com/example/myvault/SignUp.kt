7cbea322bed8accba62cd64496fcd95738c20a7f
KritikaManglam <22je05012iitism.ac.in>
2023-03-04 10:46:36 +0530

    Final Commit

--- app/src/main/java/com/example/myvault/SignUp.kt
+++ app/src/main/java/com/example/myvault/SignUp.kt
@@ -3,8 +3,12 @@ package com.example.myvault
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class SignUp : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }}
+        setContentView(R.layout.activity_sign_up)
+    }
+}
+
